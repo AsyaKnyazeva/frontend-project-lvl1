03b462b3773c6eb1328a90623c0fbf84a17c599a
AsyaKnyazeva <knyazeva478@gmail.com>
2022-05-21 13:11:01 +0300

    task4v3

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 
-import askName from '../node_modules/brain-games/src/cli.mjs';
-//const { askName } = pkg;
+import askName from 'brain-games/src/cli.mjs';
 
 console.log('Welcome to the Brain Games!');
 askName();
