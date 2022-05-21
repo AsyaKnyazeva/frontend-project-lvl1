45f73a85cb6a6644c2ac445187a218e4c4db25cc
AsyaKnyazeva <knyazeva478@gmail.com>
2022-05-21 21:01:01 +0300

    task4v9.3

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import askName from '/home/pinkbess/frontend-project-lvl1/src/cli.js';
+import askName from '../src/cli.js';
 
 console.log('Welcome to the Brain Games!');
 askName();
