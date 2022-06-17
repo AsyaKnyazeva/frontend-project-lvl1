2ad2b4904bcea6297aa8fd3c2ace056d1f8d26b2
AsyaKnyazeva <knyazeva478@gmail.com>
2022-06-17 18:05:38 +0300

    with_progression

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,56 +1,6 @@
 #!/usr/bin/env node
 
-import readlineSync from 'readline-sync';
-import askName from '../src/cli.js';
+import { rules, makeRound } from '../src/games/progression.js';
+import createGame from '../src/index.js';
 
-console.log('Welcome to the Brain Games!');
-
-const name = askName();
-
-const descr1 = 'What number is missing in the progression?';
-console.log(descr1);
-const getProgression = () => {
-  const arrInt = [];
-  let startNumber = Math.round(Math.random() * 100);
-  const quantity = 10;
-  const stepFunc = Math.round(Math.random() * 10);
-  let i = 0;
-  const index = Math.round(Math.random(0, 9) * 10);
-  let startNumberOne = startNumber;
-  const memNumber = startNumber + (index + 1) * stepFunc;
-  // console.log(startNumber, stepFunc, index, memNumber);
-  while (i < quantity) {
-    if (i === index) {
-      const nextNum = startNumber + stepFunc;
-      startNumberOne = nextNum;
-      startNumber = '..';
-    } else {
-      startNumber += stepFunc;
-      startNumberOne = startNumber;
-    }
-    arrInt.push(startNumber);
-    startNumber = startNumberOne;
-    i += 1;
-  }
-  const arrGcd = [arrInt, memNumber];
-  return arrGcd;
-};
-
-let count = 0;
-for (let i = 1; i <= 3; i += 1) {
-  const firstQuest = getProgression();
-  console.log('Question:', firstQuest[0]);
-  const answerFirst = readlineSync.question('Your answer:');
-  if (firstQuest[1] === Number(answerFirst)) {
-    console.log('Correct!');
-    count += 1;
-  } else {
-    console.log(`'${answerFirst}' was wrong. ;(. Correct answer was '${firstQuest[1]}'`);
-    console.log(`Let's try again, ${name}!`);
-    break;
-  }
-  if (count === 3) {
-    console.log(`Congratulations, ${name}!`);
-    break;
-  }
-}
+createGame(makeRound, rules);
---
+++ src/games/progression.js
@@ -0,0 +1,27 @@
+import rand from '../rand.js';
+
+const rules = 'What number is missing in the progression?';
+
+const getProgression = (length, start, stepFunc) => {
+  const progr = [];
+  let startNumber = start;
+  for (let i = 0; i < length; i += 1) {
+    progr.push(startNumber);
+    startNumber += stepFunc;
+  }
+  return progr;
+};
+
+const makeRound = () => {
+  const progrLength = rand(5, 10);
+  const progrStep = rand(5, 10);
+  const progrStart = rand(5, 10);
+  const progression = getProgression(progrLength, progrStart, progrStep);
+  const index = rand(0, progrLength - 1);
+  const correctAnswer = String(progression[index]);
+  progression[index] = '..';
+  const statement = progression.join(' ');
+  return [correctAnswer, statement];
+};
+
+export { makeRound, rules };
